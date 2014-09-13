76f742140116c16420caed090b65ba6f9fb71385
Dmitry Bogomolov <Dimones951@yandex.ru>
2014-09-14 00:07:27 +0700

    added Lab 5 IT
    Not finished lab

---
+++ Lab/IT/Lab5Progr/Lab1Progr/TaskList.h
@@ -0,0 +1,20 @@
+//
+//  TaskList.h
+//  Lab4Progr
+//
+//  Created by Дмитрий Богомолов on 13.09.14.
+//  Copyright (c) 2014 Дмитрий Богомолов. All rights reserved.
+//
+
+#ifndef Lab4Progr_TaskList_h
+#define Lab4Progr_TaskList_h
+class TaskList: public Task {
+    
+    enum STATE {
+        BEGIN,IN_WORK,END
+    };
+    
+public:
+dsflsdhfosdhklxvopjsdcpjasedgjpa};
+
+#endif
